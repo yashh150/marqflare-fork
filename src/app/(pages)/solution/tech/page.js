8ff5291d6dda956871e425ddff763f18fa271d66
shkuls <diy2903@gmail.com>
2024-08-17 14:49:01 +0530

    solutions

--- src/app/(pages)/solution/tech/page.js
+++ src/app/(pages)/solution/tech/page.js
@@ -1,5 +1,46 @@
 import Image from "next/image"
 import techHero from "@/../public/assets/techHero.svg"
+import MainSolutionCard from "@/app/components/SolutionCard/MainSolutionCard"
+const solnArray = [
+  {
+    title : "Custom Web \nDevelopment",
+    desc : "Our team of strategists, designers, \nvideo editors, and animators offers \ncomprehensive solutions to \nenhance your online presence and \nmeet your business.", 
+    imgLight : "../assets/developerLight.svg" , 
+    imgDark : "../assets/developerDark.svg"
+  },
+  {
+    title : "Website \nPersonalisation",
+    desc : "Our team of strategists, designers, \nvideo editors, and animators offers \ncomprehensive solutions to \nenhance your online presence and \nmeet your business.", 
+    imgLight : "../assets/websiteLight.svg" , 
+    imgDark : "../assets/websiteDark.svg"
+  },
+  {
+    title : "UI/UX Design \n& Research",
+    desc : "Our team of strategists, designers, \nvideo editors, and animators offers \ncomprehensive solutions to \nenhance your online presence and \nmeet your business.", 
+    imgLight : "../assets/designLight.svg" , 
+    imgDark : "../assets/designDark.svg"
+  },
+  {
+    title : "Search Engine \nOptimisation",
+    desc : "Our team of strategists, designers, \nvideo editors, and animators offers \ncomprehensive solutions to \nenhance your online presence and \nmeet your business.", 
+    imgLight : "../assets/developerLight.svg" , 
+    imgDark : "../assets/developerDark.svg"
+  },
+  {
+    title : "Email \nMarketing",
+    desc : "Our team of strategists, designers, \nvideo editors, and animators offers \ncomprehensive solutions to \nenhance your online presence and \nmeet your business.", 
+    imgLight : "../assets/notificationLight.svg" , 
+    imgDark : "../assets/notificationDark.svg"
+  },
+  {
+    title : "Marketing \nAutomation",
+    desc : "Our team of strategists, designers, \nvideo editors, and animators offers \ncomprehensive solutions to \nenhance your online presence and \nmeet your business.", 
+    imgLight : "../assets/adjustLight.svg" , 
+    imgDark : "../assets/adjustDark.svg"
+  },
+
+
+]
 
 export default function page() {
   return (
@@ -20,6 +61,27 @@ export default function page() {
          <br/>
           creation and integrated flagship campaigns.
       </p>
+      <div className="flex flex-col max-w-[1200px] m-auto">
+      <div className="flex flex-row">
+
+      {
+        solnArray.map((soln , index)=>
+           { if(index<3)
+            return(<MainSolutionCard key={index} soln={soln}/>)}
+        )
+      }
+      </div>
+      <div className="flex flex-row justify-around">
+
+      {
+        solnArray.map((soln , index)=>
+           { if(index>=3 && index<6)
+            return(<MainSolutionCard key={index} soln={soln}/>)}
+        )
+      }
+      </div>
+     
+      </div>
 
     </div>
   )
