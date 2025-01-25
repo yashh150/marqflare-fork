42f6d6c4ddab83abde8ade37063f3d6cbfe4db9d
shkuls <diy2903@gmail.com>
2025-01-25 20:53:01 +0000

    changes

--- src/app/(pages)/industry/politics/page.js
+++ src/app/(pages)/industry/politics/page.js
@@ -7,10 +7,7 @@ export default function page() {
     <div>
       <h1 className="w-[90%] mx-auto text-center mt-[50px] text-[#2E3224] text-5xl md:text-[3rem] font-semibold leading-normal">Marqflare & Politics</h1>
       <p className='md:w-1/2  w-3/4 mx-auto mt-[12px] mb-[40px] text-center text-[#464b37] text-sm font-normal leading-normal'>
-      Our team of data science experts, marketing specialists, and business consultants excel in crafting data-
-      <br/>driven electoral strategies. We provide tailored guidance to clients, leveraging technology to build and 
-      <br/>execute targeted election campaigns. Our services encompass strategic consultation, content 
-      <br/>dissemination, and amplification via programmatic advertising.
+      Our team of experts delivers data-driven electoral strategies, offering tailored consulting, targeted election campaigns, and programmatic advertising to drive success. We leverage technology and analytics to optimize political campaigns for maximum impact and engagement.
       </p>
 <Card/>
 <CardRev/>
